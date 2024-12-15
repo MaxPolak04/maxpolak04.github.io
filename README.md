cba1d6c68604ee5f47926e2d23f94786ac0245b4
Maks <max.polak04@gmail.com>
2024-12-15 21:47:46 +0100

    big changes

---
+++ README.md
@@ -0,0 +1,3 @@
+pliki strony kuzniabillert.pl
+
+hasło do servera FTP: Tufvep-qedbi1-minqih
--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,9 @@ const burgerBtn = document.querySelector('.nav__btn-burger')
 const xBtn = document.querySelector('.nav__btn-x')
 const navMobile = document.querySelector('.nav__mobile')
 const navLinks = document.querySelectorAll('.nav__mobile-link')
+const msgStatus = document.querySelector('.msg-status')
+const form = document.querySelector('.contact__form')
+let statusText = document.querySelector('.contact__info')
 
 const handleNav = () => {
     navMobile.classList.toggle('nav__mobile--active')
@@ -35,3 +38,31 @@ handleCurrentYear()
 
 burgerBtn.addEventListener('click', handleNav)
 xBtn.addEventListener('click', handleNav)
+
+form.onsubmit = (e) => {
+    e.preventDefault()
+    statusText.style.color = '#fff'
+    statusText.style.display = 'block'
+	statusText.innerText = "Wysyłanie..."
+	form.classList.add("disabled")
+
+    let xhr = new XMLHttpRequest()
+    xhr.open('POST', 'message.php', true)
+    xhr.onload = () => {
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            let response = xhr.response
+            if(response.indexOf("Pole email i wiadomość są wymagane!") != -1 || response.indexOf("Wprowadź poprawny adres email!") || response.indexOf("Błąd przy wysyłaniu wiadomości!")) {
+                statusText.style.color = '#aa3848'
+            } else {
+                form.reset()
+                setTimeout(() => {
+                    statusText.style.display = 'none'
+                }, 3000)
+            }
+            statusText.innerText = response
+			form.classList.remove("disabled")
+        }
+    }
+    let formData = new FormData(form)
+    xhr.send(formData)
+}
